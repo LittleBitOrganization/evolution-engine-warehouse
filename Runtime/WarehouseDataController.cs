28c973e328b8ec67a68f001e809a5106bc261e2a
TerentevVlad <ter.vlad.ter@gmail.com>
2022-06-27 21:32:02 +0300

    Фикс каста листа

--- Runtime/WarehouseDataController.cs
+++ Runtime/WarehouseDataController.cs
@@ -40,7 +40,7 @@ namespace LittleBit.Modules.Warehouse
 
         internal IReadOnlyList<IResourceConfig> GetAllResourceConfigsInSlots()
         {
-            return (IReadOnlyList<IResourceConfig>) _slotDataControllers.Select((pair => pair.Key));
+            return _slotDataControllers.Select((pair => pair.Key)).ToList();
         }
 
         public string Serialize() =>
