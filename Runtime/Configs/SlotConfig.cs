11b5415338c068cb19bd18514c91c63314a59b03
TerentevVlad <ter.vlad.ter@gmail.com>
2022-08-03 17:06:46 +0300

    Фикс конфига слота

--- Runtime/Configs/SlotConfig.cs
+++ Runtime/Configs/SlotConfig.cs
@@ -11,7 +11,7 @@ namespace LittleBit.Modules.Warehouse.Configs
     {
         [SerializeField] private ResourceConfig _resource;
         [SerializeField] private bool isInfinity = false;
-        [SerializeField, HideIf(nameof(IsInfinity))] private double _capacity;
+        [SerializeField, HideIf(nameof(IsInfinity)), AllowNesting] private double _capacity;
 
         public bool IsInfinity => isInfinity;
 
@@ -22,8 +22,6 @@ namespace LittleBit.Modules.Warehouse.Configs
         }
         public IResourceConfig ResourceConfig => _resource;
         public double Capacity => isInfinity? Double.MaxValue : _capacity;
-        
-        
         
         
     }
