c60be04d38090ce1ce2d2129d9dc063ea230fb13
TerentevVlad <ter.vlad.ter@gmail.com>
2022-08-03 16:58:08 +0300

    Добавил бесконечные слоты. 1.1.0

--- Runtime/Configs/SlotConfig.cs
+++ Runtime/Configs/SlotConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using LittleBit.Modules.Description;
 using LittleBit.Modules.Description.ResourceGenerator;
+using NaughtyAttributes;
 using UnityEngine;
 
 namespace LittleBit.Modules.Warehouse.Configs
@@ -9,8 +10,10 @@ namespace LittleBit.Modules.Warehouse.Configs
     public class SlotConfig
     {
         [SerializeField] private ResourceConfig _resource;
-        [SerializeField] private double _capacity;
+        [SerializeField] private bool isInfinity = false;
+        [SerializeField, HideIf(nameof(IsInfinity))] private double _capacity;
 
+        public bool IsInfinity => isInfinity;
 
         public SlotConfig(ResourceConfig resourceConfig, double capacity)
         {
@@ -18,7 +21,7 @@ namespace LittleBit.Modules.Warehouse.Configs
             _capacity = capacity;
         }
         public IResourceConfig ResourceConfig => _resource;
-        public double Capacity => _capacity;
+        public double Capacity => isInfinity? Double.MaxValue : _capacity;
         
         
         
